420f4972fdf17fb90aa4bd16a6d754bacb14f4eb
AlessioDiPretoro <alessiodipretoro@gmail.com>
2024-03-01 18:30:12 +0100

    new-NoteModal

--- website/modals.py
+++ website/modals.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from .models import Note
+import asyncio
 
 modals = Blueprint('modals', __name__)
 
-@modals.route('/new_note_modal')
-def new_note_modal():
-    return render_template('modals/new_note_modal.html')
+
+@modals.route('/note_modal/<int:id>', methods=['GET', 'POST', 'PUT'])
+@modals.route('/note_modal', methods=['GET', 'POST'])
+def note_modal(id=None):
+    if request.method == 'GET':
+        note = None
+        if id is not None:
+            note = Note.query.get(id)  
+            print("note.data " + note.data)
+    return render_template('modals/note_modal.html', note=note)
